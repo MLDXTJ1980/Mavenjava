1e11e5529a7d51e0d1db9ffa8383e4fa5c6db46a
unknown <tom1@gmail.com>
2022-02-24 13:06:03 +0100

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;//**not import org.testng.annotations.Test; or the testNG can not be trigerd
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid-01");	
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps-02");		
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;   
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation-05");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI-06");
+	}
+
+}
